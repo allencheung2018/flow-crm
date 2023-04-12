9f1e0e3d66f06116638e37d4ce7e5ec5b5b6e816
allen <allencheung11@gmail.com>
2023-04-11 23:38:33 -0400

    Database access

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,6 +1,7 @@
 package com.example.application.views.list;
 
 import com.example.application.data.entity.Contact;
+import com.example.application.data.service.CrmService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -23,14 +24,17 @@ public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
     ContactForm contactForm;
+    CrmService crmService;
 
-    public ListView() {
+    public ListView(CrmService service) {
+        this.crmService = service;
         addClassName("list-view");
         setSizeFull();
         configureGrid();
         configureForm();
 
         add(getToolbar(), getContent());
+        updateList();
     }
 
     private Component getContent() {
@@ -55,6 +59,7 @@ public class ListView extends VerticalLayout {
         filterText.setPlaceholder("Filter by name...");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
 
         Button addContactButton = new Button("Add contact");
 
@@ -64,7 +69,11 @@ public class ListView extends VerticalLayout {
     }
 
     private void configureForm() {
-        contactForm = new ContactForm(Collections.emptyList(), Collections.emptyList());
+        contactForm = new ContactForm(crmService.findAllCompanies(), crmService.findAllStatuses());
         contactForm.setWidth("25em");
+    }
+
+    private void updateList() {
+        grid.setItems(crmService.findAllContacts(filterText.getValue()));
     }
 }
