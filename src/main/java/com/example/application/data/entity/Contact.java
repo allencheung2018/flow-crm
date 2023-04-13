9bc8c55e5561a0d654399bfeef98651a5dabe772
allen <allencheung11@gmail.com>
2023-04-12 23:53:44 -0400

    Forms and Validation Handling view state

--- src/main/java/com/example/application/data/entity/Contact.java
+++ src/main/java/com/example/application/data/entity/Contact.java
@@ -9,7 +9,8 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-public class Contact extends AbstractEntity {
+public class
+Contact extends AbstractEntity {
 
     @NotEmpty
     private String firstName = "";
--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -35,6 +35,7 @@ public class ListView extends VerticalLayout {
 
         add(getToolbar(), getContent());
         updateList();
+        closeEditor();
     }
 
     private Component getContent() {
@@ -53,6 +54,9 @@ public class ListView extends VerticalLayout {
         grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
         grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
     }
 
     private HorizontalLayout getToolbar() {
@@ -62,6 +66,7 @@ public class ListView extends VerticalLayout {
         filterText.addValueChangeListener(e -> updateList());
 
         Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
 
         var toolbar = new HorizontalLayout(filterText, addContactButton);
         toolbar.addClassName("toolbar");
@@ -71,9 +76,45 @@ public class ListView extends VerticalLayout {
     private void configureForm() {
         contactForm = new ContactForm(crmService.findAllCompanies(), crmService.findAllStatuses());
         contactForm.setWidth("25em");
+        contactForm.addSaveListener(this::saveContact);
+        contactForm.addDeleteListener(this::deleteContact);
+        contactForm.addCloseListener(e -> closeEditor());
+    }
+
+    private void saveContact(ContactForm.SaveEvent event) {
+        crmService.saveContact(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        crmService.deleteContact(event.getContact());
+        updateList();
+        closeEditor();
     }
 
     private void updateList() {
         grid.setItems(crmService.findAllContacts(filterText.getValue()));
+    }
+
+    public void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            contactForm.setContact(contact);
+            contactForm.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private void closeEditor() {
+        contactForm.setContact(null);
+        contactForm.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 }
